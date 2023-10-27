9c69b6131a3937a9e6e09946de9413e072814d1d
My12123 <49092250+My12123@users.noreply.github.com>
2023-10-27 21:51:15 +0300

    Update Installer for Windows. Open for reading in the editor.bat

--- Installer for Windows. Open for reading in the editor.bat
+++ Installer for Windows. Open for reading in the editor.bat
@@ -1,3 +1,8 @@
+@rem Install python 3.10
+@rem Pre-installation steps copy files from TensorRT-8.6.0.12\onnx_graphsurgeon and TensorRT-8.6.0.12\python and place in .\
+@rem This installer supports TensorRT-8.6.0.12 for now but you can change that to whatever version you need.
+@rem Post-Installation Steps: Download and copy files from C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA\v11.8\bin and TensorRT\lib to .\venv\Scripts\
+@rem This is necessary so as not to add to PATH.
 python -m venv venv
 call .\venv\Scripts\activate
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
