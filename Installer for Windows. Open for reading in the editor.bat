df16caa3f1a5ad5132653bc211a34301dcee05a1
My12123 <49092250+My12123@users.noreply.github.com>
2023-10-27 21:50:41 +0300

    Update Installer for Windows. Open for reading in the editor.bat

--- Installer for Windows. Open for reading in the editor.bat
+++ Installer for Windows. Open for reading in the editor.bat
@@ -1,6 +1,6 @@
 python -m venv venv
 call .\venv\Scripts\activate
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
 pip install onnx_graphsurgeon-0.3.12-py2.py3-none-any.whl
 pip install tensorrt_dispatch-8.6.0-cp310-none-win_amd64.whl
