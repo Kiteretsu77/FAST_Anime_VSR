861be94e24a6a354c04c168068221d0749c9b614
My12123 <49092250+My12123@users.noreply.github.com>
2023-10-27 21:50:10 +0300

    Add files via upload

---
+++ Installer for Windows. Open for reading in the editor.bat
@@ -0,0 +1,11 @@
+python -m venv venv
+call .\venv\Scripts\activate
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install -r requirements.txt
+pip install onnx_graphsurgeon-0.3.12-py2.py3-none-any.whl
+pip install tensorrt_dispatch-8.6.0-cp310-none-win_amd64.whl
+pip install tensorrt_lean-8.6.0-cp310-none-win_amd64.whl
+pip install tensorrt-8.6.0-cp310-none-win_amd64.whl
+git clone https://github.com/NVIDIA-AI-IOT/torch2trt
+cd torch2trt
+call python setup.py install
