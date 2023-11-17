2892914166c77a6aaefe01e7a8493cec1aa98fb0
My12123 <49092250+My12123@users.noreply.github.com>
2023-11-17 21:58:08 +0300

    Update Installer for Windows. Open for reading in the editor.bat

--- Windows_Installation_script/Installer for Windows. Open for reading in the editor.bat
+++ Windows_Installation_script/Installer for Windows. Open for reading in the editor.bat
@@ -13,6 +13,4 @@ pip install onnx_graphsurgeon-0.3.12-py2.py3-none-any.whl
 pip install tensorrt_dispatch-8.6.0-cp310-none-win_amd64.whl
 pip install tensorrt_lean-8.6.0-cp310-none-win_amd64.whl
 pip install tensorrt-8.6.0-cp310-none-win_amd64.whl
-git clone https://github.com/NVIDIA-AI-IOT/torch2trt
-cd torch2trt
-call python setup.py install
+call pip install git+https://github.com/NVIDIA-AI-IOT/torch2trt.git
