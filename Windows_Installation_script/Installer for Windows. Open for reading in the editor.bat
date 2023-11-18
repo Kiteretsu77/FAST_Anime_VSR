c4c69379e879ded2bae87911036c05a0b07fbe24
My12123 <49092250+My12123@users.noreply.github.com>
2023-11-18 13:59:13 +0300

    Update Installer for Windows. Open for reading in the editor.bat

--- Windows_Installation_script/Installer for Windows. Open for reading in the editor.bat
+++ Windows_Installation_script/Installer for Windows. Open for reading in the editor.bat
@@ -10,7 +10,5 @@ call .\venv\Scripts\activate
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
 pip install -r requirements.txt
 pip install onnx_graphsurgeon-0.3.12-py2.py3-none-any.whl
-pip install tensorrt_dispatch-8.6.0-cp310-none-win_amd64.whl
-pip install tensorrt_lean-8.6.0-cp310-none-win_amd64.whl
 pip install tensorrt-8.6.0-cp310-none-win_amd64.whl
 call pip install git+https://github.com/NVIDIA-AI-IOT/torch2trt.git
